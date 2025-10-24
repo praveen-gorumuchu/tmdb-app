4bc73d5128812d7486a2ee5d27bba40cde650860
Praveen Gorumuchu <45392749+praveen-gorumuchu@users.noreply.github.com>
2025-10-25 03:20:46 +0530

    build erorr fix

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
-import "./Header.scss";
+import "./header.scss";
 import { MenuConstant } from "@/constants/menu-constant";
 import { useEffect, useState } from "react";
 import { NumberConstant } from "@/constants/Numbers.constant";
